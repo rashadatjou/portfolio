b5419e102c35d3f53e5dc10773a78521ccba1833
Mehdi <rashadatjou.mehdi@gmail.com>
2022-11-15 14:32:02 +0100

    [FEATURE] Added menu to intro

--- app/containers/Intro.tsx
+++ app/containers/Intro.tsx
@@ -9,25 +9,28 @@
  */
 
 import { LinkDescriptor } from "@remix-run/node";
-import ButtonPrimary, {
-  links as buttonLinks,
-} from "~/components/button/ButtonPrimary";
-
 import introCssPath from "~/styles/container/intro.css";
 
 // - Components
+const Menu = () => {
+  return (
+    <div className="intro__menu">
+      <ul className="intro__menu-list">
+        <li>Blog</li>
+        <li>Contact</li>
+        <li>Theme</li>
+        <li>Language</li>
+      </ul>
+    </div>
+  );
+};
+
 const IntroContainer = () => {
   return (
-    <div className="intro_container">
+    <div className="intro">
       <h1>Hello, My name is Mehdi 👋🏽</h1>
-      <span>
-        To start, <u>try</u> pressing the button bellow 👇🏽
-      </span>
-      <form action="get">
-        <ButtonPrimary className="intro-container__action-button">
-          Start
-        </ButtonPrimary>
-      </form>
+      <span>Check out the options bellow for more info 👇🏽</span>
+      <Menu />
     </div>
   );
 };
@@ -35,6 +38,5 @@ const IntroContainer = () => {
 // - Exports
 export default IntroContainer;
 export const links: LinkDescriptor[] = [
-  ...buttonLinks,
   { rel: "stylesheet", href: introCssPath },
 ];
