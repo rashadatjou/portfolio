f686b4917cfc80c47ee97eecaefcceae47308d9a
Mehdi <rashadatjou.mehdi@gmail.com>
2022-11-22 14:51:31 +0100

    [FIX] added default export to english if localization for desired lang is not found;
    [FEATURE] Added docs for blog;

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -14,23 +14,37 @@ import type { MDXModule, Post } from "~/typings/blog";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
-import blogPosts from "./__posts/posts";
+import blogModules from "./__posts/posts";
 import BlogContainer, { links as blogLinks } from "~/containers/Blog";
 import { getLocale } from "~/services/i18n/i18n.server";
 
 // - Helpers
 function postFromModule(module: MDXModule): Post {
   return {
-    slug: `${module.attributes.meta.name}/${module.filename.replace(/\.mdx?$/, "")}`,
+    slug: `${module.attributes.meta.name}/${module.filename.replace(
+      /\.mdx?$/,
+      "",
+    )}`,
     ...module.attributes.meta,
   };
+}
+
+function getLocalizedModule(locale: string, module: Record<string, MDXModule>) {
+  if (module?.[locale]) {
+    return module[locale];
+  }
+
+  // Default english
+  return module.en;
 }
 
 // - Route Module API
 export async function loader({ request }: LoaderArgs) {
   const locale = await getLocale(request);
-  const localizedBlogs = blogPosts.map((item: any) => item[locale]);
-  return json(localizedBlogs.map((blog) => postFromModule(blog as any)));
+  const localizedBlogs = blogModules.map((module: any) =>
+    getLocalizedModule(locale, module),
+  );
+  return json(localizedBlogs.map(postFromModule));
 }
 
 export const links: LinksFunction = () => {
