d4f605098a6b27751a0f399cf90db11b159c9680
Mehdi <rashadatjou.mehdi@gmail.com>
2022-12-30 15:13:41 +0100

    [UPDATE] Link list about;

--- app/views/About/index.tsx
+++ app/views/About/index.tsx
@@ -34,7 +34,7 @@ type Props = {
 // - Const
 const linkListData: LinkListItem[] = [
   { name: "Projects", href: "projects" },
-  { name: "Resume/CV", href: "resume" },
+  { name: "Resumes", href: "resume" },
   { name: "Links", href: "links" },
 ];
 
