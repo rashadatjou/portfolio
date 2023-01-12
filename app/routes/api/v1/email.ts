69cb54d14980a747fd5e4d58fa2afe2a8892d59a
Mehdi <rashadatjou.mehdi@gmail.com>
2023-01-12 20:06:44 +0100

    [UPDATE] basic setup of email api;

--- app/routes/api/v1/email.ts
+++ app/routes/api/v1/email.ts
@@ -8,10 +8,85 @@
  * -----
  */
 
-import { ActionFunction } from "@remix-run/node";
+import type { TFunction } from "i18next";
+import type { ActionFunction } from "@remix-run/node";
 
-export const loader: ActionFunction = async ({ request }) => {
-  const formData = await request.formData();
-  // TODO: Handle post request
-  return "";
+import {
+  validateEmail,
+  validateRange,
+  validateString,
+} from "~/services/validation.service";
+import type { ActionData } from "~/views/Contact";
+import { i18nRemix } from "~/services/i18n/i18n.server";
+import { badRequest } from "~/utils/helper.server";
+import { json } from "@remix-run/node";
+import { EMAIL_FIELD, MESSAGE_FIELD } from "~/views/Contact";
+import SendGrid from "@sendgrid/mail";
+
+SendGrid.setApiKey(process.env.SENDGRID_API_KEY ?? "");
+
+const composeEmail = (senderEmail: string, message: string) => ({
+  to: "test@example.com", // Change to your recipient
+  from: senderEmail, // Change to your verified sender
+  subject: "Portfolio website support",
+  text: message,
+});
+
+function validateFields(
+  email: unknown,
+  message: unknown,
+  translate: TFunction,
+) {
+  const fieldErrors = new Map<string, string>();
+
+  if (!validateEmail(email)) {
+    fieldErrors.set(EMAIL_FIELD, translate("contact.form.email.error.1"));
+  } else if (!validateRange(email as string, [4, 100])) {
+    fieldErrors.set(EMAIL_FIELD, translate("contact.form.email.error.2"));
+  }
+
+  if (!validateString(message)) {
+    fieldErrors.set(MESSAGE_FIELD, translate("contact.form.message.error.1"));
+  } else if (!validateRange(message as string, [4, 500])) {
+    fieldErrors.set(MESSAGE_FIELD, translate("contact.form.message.error.2"));
+  }
+
+  return fieldErrors;
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  const translate = await i18nRemix.getFixedT(request, "base");
+  const form = await request.formData();
+  const email = form.get(EMAIL_FIELD);
+  const message = form.get(EMAIL_FIELD);
+
+  if (typeof email !== "string" || typeof message !== "string") {
+    return badRequest<ActionData>({
+      formError: translate("contact.form.failure"),
+    });
+  }
+
+  const errors = validateFields(email, message, translate);
+
+  if (errors.size > 0) {
+    return badRequest<ActionData>({
+      fields: { email, message },
+      fieldErrors: {
+        email: errors.get(EMAIL_FIELD),
+        message: errors.get(MESSAGE_FIELD),
+      },
+    });
+  }
+
+  // TODO: Turn this on
+  // try {
+  //   const composedEmail = composeEmail(email, message);
+  //   await SendGrid.send(composedEmail);
+  //   console.log("Email sent");
+  // } catch (error) {
+  //   console.error(error);
+  // }
+
+  // TODO: Redirect back to about page with proper response
+  return json({ message: translate("contact.form.success") }, { status: 200 });
 };
--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -8,89 +8,20 @@
  * -----
  */
 
-import type { TFunction } from "i18next";
-import type { LinksFunction, ActionFunction } from "@remix-run/node";
-import type { ActionData } from "~/views/Contact";
-
-import { json } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 import { useActionData } from "@remix-run/react";
-import { i18nRemix } from "~/services/i18n/i18n.server";
-import { badRequest } from "~/utils/helper.server";
-import {
-  validateEmail,
-  validateRange,
-  validateString,
-} from "~/services/validation.service";
 import { useTranslation } from "react-i18next";
-
-import ContactView, {
-  EMAIL_FIELD,
-  MESSAGE_FIELD,
-  links as contactViewLinks,
-} from "~/views/Contact";
-
-// - Methods
-function validateFields(
-  email: unknown,
-  message: unknown,
-  translate: TFunction,
-) {
-  const fieldErrors = new Map<string, string>();
-
-  if (!validateEmail(email)) {
-    fieldErrors.set(EMAIL_FIELD, translate("contact.form.email.error.1"));
-  } else if (!validateRange(email as string, [4, 100])) {
-    fieldErrors.set(EMAIL_FIELD, translate("contact.form.email.error.2"));
-  }
-
-  if (!validateString(message)) {
-    fieldErrors.set(MESSAGE_FIELD, translate("contact.form.message.error.1"));
-  } else if (!validateRange(message as string, [4, 500])) {
-    fieldErrors.set(MESSAGE_FIELD, translate("contact.form.message.error.2"));
-  }
-
-  return fieldErrors;
-}
+import ContactView, { links as contactViewLinks } from "~/views/Contact";
 
 // - Route Module API
 export const links: LinksFunction = () => {
   return [...contactViewLinks];
 };
 
-export const action: ActionFunction = async ({ request }) => {
-  const translate = await i18nRemix.getFixedT(request, "base");
-  const form = await request.formData();
-  const email = form.get(EMAIL_FIELD);
-  const message = form.get(EMAIL_FIELD);
-
-  if (typeof email !== "string" || typeof message !== "string") {
-    return badRequest<ActionData>({
-      formError: translate("contact.form.failure"),
-    });
-  }
-
-  const errors = validateFields(email, message, translate);
-
-  if (errors.size > 0) {
-    return badRequest<ActionData>({
-      fields: { email, message },
-      fieldErrors: {
-        email: errors.get(EMAIL_FIELD),
-        message: errors.get(MESSAGE_FIELD),
-      },
-    });
-  }
-
-  // TODO: Send email via a service;
-
-  return json({ message: translate("contact.form.success") }, { status: 200 });
-};
-
 // - Components
 const ContactRoute = () => {
   const actionData = useActionData();
   const { t } = useTranslation();
-
   return <ContactView t={t} data={actionData} />;
 };
 
