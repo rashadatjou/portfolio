32151bc0644899d6d35458b0f0014a6e99f387db
Mehdi <rashadatjou.mehdi@gmail.com>
2023-01-13 18:53:36 +0100

    [UPDATE] added AMAZON SES:

--- app/routes/api/v1/email.ts
+++ app/routes/api/v1/email.ts
@@ -10,23 +10,25 @@
 
 import type { TFunction } from "i18next";
 import type { ActionFunction } from "@remix-run/node";
+import type { ActionData } from "~/views/Contact";
 
 import {
   validateEmail,
   validateRange,
   validateString,
 } from "~/services/validation.service";
-import type { ActionData } from "~/views/Contact";
 import { i18nRemix } from "~/services/i18n/i18n.server";
 import { badRequest } from "~/utils/helper.server";
 import { json } from "@remix-run/node";
 import { EMAIL_FIELD, MESSAGE_FIELD } from "~/views/Contact";
 
+import emailService from "~/services/email.service";
+
 const composeEmail = (senderEmail: string, message: string) => ({
-  to: "test@example.com", // Change to your recipient
-  from: senderEmail, // Change to your verified sender
+  to: process.env.AWS_SMTP_EMAIL_TO, // Change to your recipient
+  from: process.env.AWS_SMTP_EMAIL_FORM, // Change to your verified sender
   subject: "Portfolio website support",
-  text: message,
+  text: message + senderEmail,
 });
 
 function validateFields(
@@ -55,7 +57,7 @@ export const action: ActionFunction = async ({ request }) => {
   const translate = await i18nRemix.getFixedT(request, "base");
   const form = await request.formData();
   const email = form.get(EMAIL_FIELD);
-  const message = form.get(EMAIL_FIELD);
+  const message = form.get(MESSAGE_FIELD);
 
   if (typeof email !== "string" || typeof message !== "string") {
     return badRequest<ActionData>({
@@ -75,8 +77,16 @@ export const action: ActionFunction = async ({ request }) => {
     });
   }
 
-  // TODO: Send email...
+  // Send email...
+  try {
+    let composedEmail = composeEmail(email, message);
+    console.log("composedEmail", composedEmail);
+    const res = await emailService.sendMail(composedEmail);
+    console.log("res", res);
+  } catch (error) {
+    console.log("error", error);
+  }
 
-  // TODO: Redirect back to about page with proper response
+  // Redirect back to about page with proper response
   return json({ message: translate("contact.form.success") }, { status: 200 });
 };
