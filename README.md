8c5904d4b181addd511786ce7bb8cdffd3b850ce
Mehdi <rashadatjou.mehdi@gmail.com>
2023-01-17 14:32:45 +0100

    [RELEASE] Prepared version for 1.0.0 release;

--- README.md
+++ README.md
@@ -1,20 +1,32 @@
+# Address
+
+rashadatjou.com
+
+# Hosting
+
+cloudflare.com
+
 # Quick Info
-This project should be used as a template project. The project has the following features:
+
+- Remix
 - CSS Reset
 - CSS Scaffold
-- remix-i18n integration
-- Javascript (enable/disable) detection
+- Localization
 - Typings setup
 - Theming setup
 - Custom errors
 
 # Tech Stack
-**Server**
+
+**Framework**
+
 - Remix.run [https://remix.run]
 
 **Dependencies**
+
 - React 18.2 [https://reactjs.org]
 - TypeScript 4.7.4 [https://www.typescriptlang.org]
 - i18Next (Localization) [https://react.i18next.com]
 - classnames [https://github.com/JedWatson/classnames]
 - eslint [https://github.com/eslint/eslint]
+- themeit [https://github.com/rashadatjou/themeit]
--- app/root.tsx
+++ app/root.tsx
@@ -50,21 +50,32 @@ export const loader: LoaderFunction = async ({ request }) => {
   const locale = await getLocale(request);
   const translate = await i18nRemix.getFixedT(request, "base");
   const title = translate("meta.title");
+  const description = translate("meta.description");
 
   const headers = new Headers();
   headers.set("Set-Cookie", await i18nCookie.serialize(locale));
 
-  return json({ locale, title }, { headers });
+  return json({ locale, title, description }, { headers });
 };
 
 export const meta: MetaFunction = ({ data }) => ({
   charset: "utf-8",
   title: data.title,
+  description: data.description,
   viewport: "width=device-width,initial-scale=1",
+  "application-name": "@rashadatjou",
+  generator: "remix.run"
 });
 
 export const links: LinksFunction = () => {
   return [
+    { rel: "manifest", href: "/manifest.webmanifest" },
+    { rel: "icon", href: "favicon/favicon.ico" },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "favicon/apple-touch-icon.png",
+    },
     { rel: "stylesheet", href: resetCssPath },
     {
       rel: "stylesheet",
